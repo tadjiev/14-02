eddbbd4d453fe00e9a02ddb2092585ead5b2a229
tadjiev <yrystadjiev@gmail.com>
2022-05-07 21:30:57 +0600

    lesson1

---
+++ main.js
@@ -0,0 +1,61 @@
+const   a = 'a';
+
+let c = 334
+
+const bool = true ;
+const bool2 = false;
+
+const  obj = {
+    key: "value",
+    username: "Yrys",
+    data: {
+        first_name: "Jack",
+        last_name: "Tadjiev",
+
+    }
+}
+
+const arr = []
+
+const  arr2 =[
+    {
+        key: " value"
+    },
+    {
+      key: "value"
+    },
+    {
+        key: "value3"
+    }
+]
+const first_number = prompt ("Введите число")
+const last_number = prompt ("Введите второе число")
+
+if(first_number > last_number){
+    console.log(first_number + ">" + last_number)
+}
+else if (first_number < last_number){
+    console.log(first_number + "<" + last_number)
+}
+else {
+    console.log(first_number + "=" + last_number)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
